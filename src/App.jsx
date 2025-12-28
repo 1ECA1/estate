3ea928a442983bef257c5650dc51cce8e3ac7563
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-12-28 01:14:56 +0100

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Default route */}
+        <Route path="/" element={<Home />} />
+
+        {/* Pages */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+        {/* 404 page */}
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
