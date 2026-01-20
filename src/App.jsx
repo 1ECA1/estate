be6fa05814ce176af37f30914fd37e4ad989c925
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2026-01-20 06:48:09 +0100

    create heade component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./componets/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
+
       <Routes>
         {/* Default route */}
         <Route path="/" element={<Home />} />
