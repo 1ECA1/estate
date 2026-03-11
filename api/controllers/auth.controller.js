11a112f45e170e51395f640fa01c272e698f252d
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2026-03-11 12:10:47 +0100

    create a middlewear amd a function that handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,9 @@
 import User from "../models/User.model.js"
 import bcryptjs from "bcryptjs";
-
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword =  bcryptjs.hashSync(password, 10);
-
     const newUser = new User({ username, email, password: hashedPassword });
    
     
@@ -12,13 +11,14 @@ export const signup = async (req, res) => {
     // console.log("Received data:", req.body);
 
     try {
-         await newUser.save()
+         await newUser.save();
         // This sends a response back to Postman so the request finishes
         res.status(201).json({
             message: "User created successfully!",
             data: { username, email }
         });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+    //   next(500).json({ message: error.message });
+        next(errorHandler(550, 'error from the function'));
     }
 };
